05ecf34bbfd4c9746db59db70da13e58ffc05e82
Aakash <aakashkathunia@gmial.com>
2025-09-25 22:43:01 +0530

    updated codeOIDC1

--- AKS/main.tf
+++ AKS/main.tf
@@ -1,6 +1,15 @@
+# Generate random resource group name
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = var.resource_group_name
+  name     = random_pet.rg_name.id
+}
+
+resource "random_pet" "azurerm_kubernetes_cluster_name" {
+  prefix = "cluster"
 }
 
 resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
@@ -9,8 +18,8 @@ resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
 
 resource "azurerm_kubernetes_cluster" "k8s" {
   location            = azurerm_resource_group.rg.location
-  name                = var.kubernetes_cluster_name
-  resource_group_name = var.resource_group_name
+  name                = random_pet.azurerm_kubernetes_cluster_name.id
+  resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
   kubernetes_version  = "1.30.0"
 
--- AKS/providers.tf
+++ AKS/providers.tf
@@ -30,17 +30,11 @@ terraform {
 }
 provider "azurerm" {
    features {}
-   subscription_id = "94ef7338-97fb-4eba-8042-ef97ff5cebd4"
-   tenant_id       = "07fc8f44-8a45-4e66-973d-705143cdbdd1"
-   client_id       = "8b939382-f5c5-4581-8ad0-9573ad670b19"
    use_oidc = true
 }
 
 # AzAPI provider for advanced actions like generateKeyPair
 provider "azapi" {
-  subscription_id = "94ef7338-97fb-4eba-8042-ef97ff5cebd4"
-  tenant_id       = "07fc8f44-8a45-4e66-973d-705143cdbdd1"
-  client_id       = "8b939382-f5c5-4581-8ad0-9573ad670b19"
   use_oidc = true
 }
 
